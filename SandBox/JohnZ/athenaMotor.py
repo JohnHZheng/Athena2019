6f2da37a3af328cb5710458c1fa83220b9e09ba5
John Zheng <john.h.zheng2008@gmail.com>
2019-08-25 10:03:51 -0700

    More testings

--- SandBox/JohnZ/athenaMotor.py
+++ SandBox/JohnZ/athenaMotor.py
@@ -12,7 +12,7 @@ import sys
 wheelRadiusCm = 2.75 
 wheelCircumferenceCm = 2 * math.pi * wheelRadiusCm
 
-def Run(distanceCm, speedCmPerSecond, leftMotorPort = OUTPUT_B, rightMotorPort = OUTPUT_C, brake=True, block=True):
+def run(distanceCm, speedCmPerSecond, leftMotorPort = OUTPUT_B, rightMotorPort = OUTPUT_C, brake=True, block=True):
     # Initialize Motors
     leftMotor = LargeMotor(leftMotorPort)
     rightMotor = LargeMotor(rightMotorPort)  
@@ -24,8 +24,47 @@ def Run(distanceCm, speedCmPerSecond, leftMotorPort = OUTPUT_B, rightMotorPort =
     leftMotor.on_for_degrees(SpeedDPS(speedDegreePerSecond), degreesToRun, brake, False)
     rightMotor.on_for_degrees(SpeedDPS(speedDegreePerSecond), degreesToRun, brake, block)
 
+def onUntilGameLine( white_threshold = 85, black_threshold = 30
+    leftMotorPort = OUTPUT_B, rightMotorPort = OUTPUT_C, leftSensorPort = INPUT_1, rightSensorPort = INPUT_4, brake = True)
+    # Initialize Motors and sensors
+    leftMotor = LargeMotor(leftMotorPort)
+    rightMotor = LargeMotor(rightMotorPort) 
+    leftSensor = ColorSensor(leftSensorPort)
+    rightSensor = ColorSensor(rightSensorPort)
+
+    # calculate speed to use in DPS
+    speedDegreePerSecond = speedCmPerSecond / wheelCircumferenceCm * 360
+
+    # Start motor at internal determined speed. 
+    leftMotor.on(15)
+    rightMotor.on(15) 
+
+    # flags for whether both left and right wheel are in position
+    leftStopped = False
+    rightStopped = False
+
+    while(not leftStopped or not rightStopped):
+        left_reflected = leftSensor.reflected_light_intensity
+        right_reflected = leftSensor.reflected_light_intensity
+        sleep(.005) 
+
+    #while (cs4.color != ColorSensor.COLOR_BLACK and cs1.color != ColorSensor.COLOR_BLACK):
+    """
+     while (loopCounter < 24):
+        print( "{0:02d} - CS1: {1:10}, CS4: {2:10}, CS1Reflected: {3:04d}, CS4Reflected: {3:04d}".format( 
+            loopCounter, cs1.color_name, cs4.color_name, cs1.reflected_light_intensity, cs4.reflected_light_intensity), file=sys.stderr)
+        loopCounter += 1;
+        sleep(.005) 
+    """
+
+    leftMotor.off()
+    rightMotor.off()
+
+
+
+
 # run forward 50 cm at speed of 30cm/s
-Run(50, 30)
+run(50, 30)
 sleep(0.2)
 # run backward 50 cm at speed of 20cm/s
-Run(-50, 20)
+run(-50, 20)
--- SandBox/JohnZ/testGameLine.py
+++ SandBox/JohnZ/testGameLine.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+#!/usr/bin/env micropython
 from ev3dev2.motor import LargeMotor,  OUTPUT_C, OUTPUT_B, follow_for_ms
 from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM, MoveTank, MoveSteering, SpeedPercent
 from time import sleep
@@ -19,14 +19,14 @@ lmC.on(10)
 
 loopCounter = 0
 #while (cs4.color != ColorSensor.COLOR_BLACK and cs1.color != ColorSensor.COLOR_BLACK):
-while (loopCounter < 24):
+while (loopCounter < 48):
     print( "{0:02d} - CS1: {1:10}, CS4: {2:10}, CS1Reflected: {3:04d}, CS4Reflected: {3:04d}".format( 
         loopCounter, cs1.color_name, cs4.color_name, cs1.reflected_light_intensity, cs4.reflected_light_intensity), file=sys.stderr)
     loopCounter += 1;
     sleep(.005)
 
-lmB.Off()
-lmC.Off()
+lmB.off()
+lmC.off()
     # print("CS1 - reflected:{0}, ambient:{1}, color:{2}", cs1.reflected_light_intensity, cs1.ambient_light_intensity, cs1.color_name, file=sys.stderr)
     # print("CS4 - reflected:{0}, ambient:{1}, color:{2}", cs4.reflected_light_intensity, cs4.ambient_light_intensity, cs4.color_name, file=sys.stderr)
 
