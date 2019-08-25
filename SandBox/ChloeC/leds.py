58f009280b8bf91fb4bcbb3c0a5b08e12b99eb6d
Chlooooooe <lolycheng@gmail.com>
2019-08-25 12:05:44 -0700

    Update leds.py

--- SandBox/ChloeC/leds.py
+++ SandBox/ChloeC/leds.py
@@ -2,6 +2,9 @@
 from ev3dev2.button import Button 
 from time import sleep
 from ev3dev2.led import Leds
+import os
+import sys
+os.system('setfont Lat15-TerminusBold14')
 
 btn = Button() 
 loop = True
@@ -17,3 +20,5 @@ while loop:
         leds.set_color('LEFT','AMBER')
     elif btn.enter == True:
         loop = False 
+    else:
+        print('Do something!')
