7ef1e3765ff5dd4699d72844c4f53e6f5465f633
coding000 <53977813+coding000@users.noreply.github.com>
2019-09-09 20:40:30 -0700

    new

--- SandBox/MaxWang/test.py
+++ SandBox/MaxWang/test.py
@@ -5,17 +5,8 @@ from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM, MoveTank, Move
 from time import sleep
 from ev3dev2.sensor.lego import ColorSensor
 
-lmB = LargeMotor(OUTPUT_B)
-lmC = LargeMotor(OUTPUT_C)
 
-
-A = 0
-while A<4:
-    lmB.on_for_seconds(MoveTank(-50), 2.25, True, False)
-    lmC.on_for_seconds(MoveTank(-50), 2.25)
-     lmB.on_for_seconds(MoveTank(-100), 2.25, True, False)
-    lmC.on_for_seconds(MoveTank(-50), 2.25) 
-    A = A+1
+    
     
     
 
