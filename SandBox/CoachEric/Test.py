771fcd556eca080eca1be311e79444f15b76f8c7
coding000 <53977813+coding000@users.noreply.github.com>
2019-09-14 11:39:25 -0700

    test update

--- SandBox/CoachEric/Test.py
+++ SandBox/CoachEric/Test.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env micropython
+
 from ev3dev2.motor import LargeMotor,MediumMotor, OUTPUT_D, OUTPUT_A,  OUTPUT_C, OUTPUT_B, follow_for_ms
 from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM, MoveTank, MoveSteering, SpeedPercent
 from ev3dev2.sound import Sound
 from time import sleep
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sound import Sound
 import math
 import sys 
 import time
@@ -15,7 +17,7 @@ LeftSensor      = ColorSensor(INPUT_1)
 RightSensor     = ColorSensor(INPUT_4)
 LeftWheel       = LargeMotor(OUTPUT_B)
 RightWheel      = LargeMotor(OUTPUT_C)
-
+sound           = Sound()
 def Step1():
     TankPair.on_for_seconds(SpeedDPS(-405),SpeedDPS(-400), 1.3,False,True)
     TankPair.on_for_seconds(SpeedDPS(-255),SpeedDPS(-250), 1.2,False,True)
@@ -23,10 +25,15 @@ def Step1():
 def Step2():
     TankPair.on_for_degrees(SpeedDPS(250),SpeedDPS(250), 270,True,True)
 
+ #def Step3():
+	#TankPair.on_for_degrees(SpeedDPS(0),SpeedDPS(-250),230 )
 
+#def Step4():#TankPair.on_for_degrees(SpeedDPS(-250),SpeedDPS(250), 100,True,True)
+    
 
-#print("LeftSensor - reflected:{0}, ambient:{1}, color:{2}", LeftSensor.reflected_light_intensity, LeftSensor.ambient_light_intensity, LeftSensor.color_name) #, file=sys.stderr)
- 
-Step1()
+sound.beep()
+print("LeftSensor - reflected:{0}, color:{1}", LeftSensor.reflected_light_intensity, LeftSensor.color_name, file=sys.stderr)
+print("RightSensor - reflected:{0}, color:{1}", RightSensor.reflected_light_intensity, RightSensor.color_name, file=sys.stderr)
+#Step1()
 #Step2()
 #Step3()
