c6aee43e976286f51d1d7f8cd7861c0704575a55
coding000 <53977813+coding000@users.noreply.github.com>
2019-09-14 17:44:33 -0700

    hi

--- SandBox/MaxWang/testA.py
+++ SandBox/MaxWang/testA.py
@@ -17,22 +17,32 @@ RightSensor     = ColorSensor(INPUT_4)
 LeftWheel       = LargeMotor(OUTPUT_B)
 RightWheel      = LargeMotor(OUTPUT_C)
 sound           = Sound()
+
+
 def Step1():
-    TankPair.on_for_seconds(SpeedDPS(-405),SpeedDPS(-400), 1.3,False,True)
-    TankPair.on_for_seconds(SpeedDPS(-255),SpeedDPS(-250), 1.2,False,True)
+    TankPair.on_for_seconds(SpeedDPS(450),SpeedDPS(455), 1.3,False,True)
+    TankPair.on_for_seconds(SpeedDPS(250),SpeedDPS(260), 1.2,False,True)
+def Step2():   
+    TankPair.on_for_degrees(SpeedDPS(-250),SpeedDPS(-250), 250,True,True)
+    TankPair.on_for_seconds(SpeedDPS(400),SpeedDPS(0), 0.5,True,True)
     
-def Step2():
-    TankPair.on_for_degrees(SpeedDPS(250),SpeedDPS(250), 270,True,True)
+    while LeftSensor.color != 1:
+        TankPair.on(SpeedDPS(250),SpeedDPS(250))
+    LeftWheel.off()
+    RightWheel.off()
+    while LeftSensor.color != 6:
+        TankPair.on(SpeedDPS(250),SpeedDPS(250))
+    LeftWheel.off()
+    RightWheel.off()
+    TankPair.on_for_degrees(SpeedDPS(250),SpeedDPS(250), 20,True,True)
+    TankPair.on_for_degrees(SpeedDPS(0),SpeedDPS(250), 270,True,True)
 
- #def Step3():
-	#TankPair.on_for_degrees(SpeedDPS(0),SpeedDPS(-250),230 )
 
-#def Step4():#TankPair.on_for_degrees(SpeedDPS(-250),SpeedDPS(250), 100,True,True)
-    
 
-sound.beep()
-print("LeftSensor - reflected:{0}, color:{1}", LeftSensor.reflected_light_intensity, LeftSensor.color_name, file=sys.stderr)
-print("RightSensor - reflected:{0}, color:{1}", RightSensor.reflected_light_intensity, RightSensor.color_name, file=sys.stderr)
+    #print("LeftSensor - reflected:{0}, color:{1}", LeftSensor.reflected_light_intensity, LeftSensor.color_name, file=sys.stderr) 
+    #print("RightSensor - reflected:{0}, color:{1}", RightSensor.reflected_light_intensity, RightSensor.color_name, file=sys.stderr) 
+
+
 #Step1()
-#Step2()
-#Step3()
+Step2()
+
