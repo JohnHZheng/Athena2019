6a29dde7e176f240771779392288a682c01a0151
coding000 <53977813+coding000@users.noreply.github.com>
2019-10-08 21:10:54 -0700

    hi

--- SandBox/MaxWang/testB.py
+++ SandBox/MaxWang/testB.py
@@ -17,12 +17,36 @@ RightSensor     = ColorSensor(INPUT_4)
 LeftWheel       = LargeMotor(OUTPUT_B)
 RightWheel      = LargeMotor(OUTPUT_C)
 sound           = Sound()
+def BackWard_linefollowing(FastSpeed,SlowSpeed,Degree):
+    DegreeSum = 0
+    AngleOld    = 360 * RightWheel.position / RightWheel.count_per_rot
+    while DegreeSum < Degree:
+        if RightSensor.color == ColorSensor.COLOR_WHITE:
+            LeftWheel.on(SpeedDPS(FastSpeed))
+            RightWheel.on(SpeedDPS(SlowSpeed))
+        else:
+            RightWheel.on(SpeedDPS(FastSpeed))
+            LeftWheel.on(SpeedDPS(SlowSpeed))  
+        AngleNew    = 360 * RightWheel.position / RightWheel.count_per_rot
+        DegreeSum   = DegreeSum + abs(AngleNew - AngleOld)
+        AngleOld    = AngleNew
+        #print(RightSensor.reflected_light_intensity,LeftSensor.reflected_light_intensity,file=sys.stderr)
+    LeftWheel.off()
+    RightWheel.off()
 
 def Step7():
-    TankPair.on_for_seconds(SpeedDPS(200),SpeedDPS(200), 2,True,True)
-    TankPair.on_for_seconds(SpeedDPS(300),SpeedDPS(300), 4.1,True,True)
-    TankPair.on_for_seconds(SpeedDPS(-200),SpeedDPS(-200), 0.2,True,True)
+    TankPair.on_for_degrees(SpeedDPS(200),SpeedDPS(200), 1520,True,True)
+    TankPair.on_for_degrees(SpeedDPS(-200),SpeedDPS(-200), 60,True,True)
     RightAction.on_for_degrees(-100,2100,True,True)
-    #TankPair.on_for_seconds(SpeedDPS(-300),SpeedDPS(300), 2,False,True)
+def Step8():
+    TankPair.on_for_degrees(SpeedDPS(200),SpeedDPS(200),220,True,True)
+    TankPair.on_for_degrees(SpeedDPS(200),SpeedDPS(0),200,True,True)
+    TankPair.on_for_degrees(SpeedDPS(200),SpeedDPS(200),120,True,True)
+def Step9():
+    BackWard_linefollowing(150,30,1000)
+
     
-Step7()
+
+#Step7()
+#Step8()
+Step9()
