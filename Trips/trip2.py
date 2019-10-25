5354cf488ae15192a78c1017d096d12d7077c77c
Ethan263 <53958783+Ethan263@users.noreply.github.com>
2019-10-24 22:01:36 -0700

    Integrated trip 2 + 3
    it was easy

--- Trips/trip2.py
+++ Trips/trip2.py
@@ -1,22 +1,142 @@
 #!/usr/bin/env micropython
-
-from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_C, OUTPUT_B, OUTPUT_D, follow_for_ms
+from ev3dev2.motor import LargeMotor,MediumMotor, OUTPUT_D, OUTPUT_A,  OUTPUT_C, OUTPUT_B, follow_for_ms
 from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM, MoveTank, MoveSteering, SpeedPercent
+from ev3dev2.sound import Sound
 from time import sleep
 from ev3dev2.sensor.lego import ColorSensor
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sound import Sound
 import math
 import sys 
-from athenaRobot import AthenaRobot
+import time
+LeftAction      = MediumMotor(OUTPUT_A)
+RightAction     = MediumMotor(OUTPUT_D)
+TankPair        = MoveTank(OUTPUT_B, OUTPUT_C, motor_class=LargeMotor)
+LeftSensor      = ColorSensor(INPUT_1)
+RightSensor     = ColorSensor(INPUT_4)
+LeftWheel       = LargeMotor(OUTPUT_B)
+RightWheel      = LargeMotor(OUTPUT_C)
+sound           = Sound()
 
+def LineFollowing(FastSpeed,SlowSpeed,Degree):
+    DegreeSum = 0
+    AngleOld    = 360 * RightWheel.position / RightWheel.count_per_rot
+    while DegreeSum < Degree:
+        if RightSensor.color == ColorSensor.COLOR_WHITE:
+            LeftWheel.on(SpeedDPS(FastSpeed))
+            RightWheel.on(SpeedDPS(SlowSpeed))
+        else:
+            RightWheel.on(SpeedDPS(FastSpeed))
+            LeftWheel.on(SpeedDPS(SlowSpeed))  
+        AngleNew    = 360 * RightWheel.position / RightWheel.count_per_rot
+        DegreeSum   = DegreeSum + abs(AngleNew - AngleOld)
+        AngleOld    = AngleNew
+        #print(RightSensor.reflected_light_intensity,LeftSensor.reflected_light_intensity,file=sys.stderr)
+    LeftWheel.off()
+    RightWheel.off()
+
+
+def Step1():
+    TankPair.on_for_seconds(SpeedDPS(-310),SpeedDPS(-300), 2,False,True)
+    TankPair.on_for_seconds(SpeedDPS(-180),SpeedDPS(-180), 1,False,True)
+
+def Step2():
+    TankPair.on_for_degrees(SpeedDPS(250),SpeedDPS(250), 150,True,True)
+    TankPair.on_for_degrees(SpeedDPS(400),SpeedDPS(0), 150,True,True)
+    while RightSensor.color !=1:
+        TankPair.on(SpeedDPS(-200),SpeedDPS(-200))
+    while RightSensor.color != 6:
+        TankPair.on(SpeedDPS(-200),SpeedDPS(-200))
+    LeftWheel.off()
+    RightWheel.off()
+    TankPair.on_for_degrees(SpeedDPS(-250),SpeedDPS(-250), 90,True,True)
+    while RightSensor.color !=6:
+        TankPair.on(SpeedDPS(-200),SpeedDPS(0))
+    while RightSensor.color !=1:
+        TankPair.on(SpeedDPS(-200),SpeedDPS(0))
+    LeftWheel.off()
+    RightWheel.off()
+    LineFollowing(-180,-50,150)
+    LineFollowing(-100,-0,120)
+    if RightSensor.color == 1:
+        TankPair.on_for_degrees(SpeedDPS(0),SpeedDPS(-100), 10,True,True)
+    LeftWheel.off()
+    RightWheel.off()
+    #while LeftSensor.color !=6:
+        #TankPair.on(SpeedDPS(0),SpeedDPS(-70))
+    #LeftWheel.off()
+    #RightWheel.off()
+    TankPair.on_for_degrees(SpeedDPS(-250),SpeedDPS(-250),200,True,True)
+    while LeftSensor.color !=6:
+        TankPair.on(SpeedDPS(-200),SpeedDPS(-200))
+    while LeftSensor.color !=1:
+        TankPair.on(SpeedDPS(-200),SpeedDPS(-200))
+    LeftWheel.off()
+    RightWheel.off()
+    LineFollowing(-100,-0,130)
+    TankPair.on_for_degrees(SpeedDPS(-250),SpeedDPS(-250), 90,True,True)
+    LineFollowing(-100,-0,110)
+    RightAction.on_for_degrees(50,850,True,True)
+    while RightSensor.color !=6:
+        TankPair.on(SpeedDPS(0),SpeedDPS(-100))
+    LeftWheel.off()
+    RightWheel.off()
+    TankPair.on_for_degrees(SpeedDPS(-250),SpeedDPS(-250), 130,True,True)
+    RightAction.on_for_degrees(-50,850)
+
+def Step3():  
+    TankPair.on_for_degrees(SpeedDPS(0),SpeedDPS(-250), 240,True,True)
+    TankPair.on_for_degrees(SpeedDPS(-250),SpeedDPS(-250), 240,True,True)
+    TankPair.on_for_degrees(SpeedDPS(250), SpeedDPS(0),95,True,True )
+    TankPair.on_for_degrees(SpeedDPS(250),SpeedDPS(250), 140,True,True)
+    TankPair.on_for_degrees(SpeedDPS(0), SpeedDPS(250),100,True,True )
+    RightAction.on_for_degrees(50,850,True,True)
+    TankPair.on_for_degrees(SpeedDPS(150),SpeedDPS(300), 120,True,True)
+    TankPair.on_for_degrees(SpeedDPS(-150),SpeedDPS(-150), 20,True,True)
+    RightAction.on_for_degrees(-50,425,True,True)
+    TankPair.on_for_degrees(SpeedDPS(150),SpeedDPS(150), 20,True,True)
+    TankPair.on_for_degrees(SpeedDPS(150),SpeedDPS(300), 140,True,True)
+    RightAction.on_for_degrees(-50,425,True,True)
+
+def Step4():
+    TankPair.on_for_degrees(SpeedDPS(150),SpeedDPS(0), 125,True,True)
+    TankPair.on_for_degrees(SpeedDPS(-150),SpeedDPS(-150), 20,True,True)
+    TankPair.on_for_degrees(SpeedDPS(0),SpeedDPS(-250), 140,True,True)
+    TankPair.on_for_degrees(SpeedDPS(-250),SpeedDPS(-250),600,True,True)
+    TankPair.on_for_degrees(SpeedDPS(0),SpeedDPS(300), 175,True,True)
+    while RightSensor.color !=6:
+        TankPair.on(SpeedDPS(0),SpeedDPS(150))
+    while RightSensor.color !=1: 
+        TankPair.on(SpeedDPS(0),SpeedDPS(150))
+    LeftWheel.off()
+    RightWheel.off()
+    LeftAction.on_for_degrees(-50,1200,True,False)
+    LineFollowing(-150,-20,250)
+    while RightSensor.color !=1:
+        TankPair.on(SpeedDPS(-150),SpeedDPS(0))
+    while RightSensor.color !=6:
+        TankPair.on(SpeedDPS(-150),SpeedDPS(0))
+    LeftWheel.off()
+    RightWheel.off()
+    TankPair.on_for_degrees(SpeedDPS(-250),SpeedDPS(-250),130,True,True)
+    LeftAction.on_for_degrees(50,1200,True,True)
+    #TankPair.on_for_degrees(SpeedDPS(0),SpeedDPS(250),25,True,True)
+
+def Step5():
+    TankPair.on_for_degrees(SpeedDPS(500),SpeedDPS(500),1680,False,True)
+    TankPair.on_for_degrees(SpeedDPS(0),SpeedDPS(250),95,True,True)
+    TankPair.on_for_degrees(SpeedDPS(500),SpeedDPS(500),1080,True,True)
 def runTrip2():
-        robot = AthenaRobot()
-        # with blue unit perfect up and down of forklift
-        robot.moveMediumMotor(isLeft = False, speed = -30, degrees = 900)
-        sleep(1)
-        robot.moveMediumMotor(isLeft = False, speed = 50, degrees = 510)
+    Step1()
+    Step2()
+    Step3()
+    Step4()
+    Step5()
 
-        # without blue unit perfect up and down of forklift
-        robot.moveMediumMotor(isLeft = False, speed = -30, degrees = 600)
-        sleep(1)
-        robot.moveMediumMotor(isLeft = False, speed = 50, degrees = 510)
+
+
+
+
+
+
+
--- Trips/trip3.py
+++ Trips/trip3.py
@@ -1,50 +1,88 @@
 #!/usr/bin/env micropython
-from ev3dev2.motor import LargeMotor, MoveTank, OUTPUT_C, OUTPUT_B
-from ev3dev2.sensor import INPUT_1, INPUT_4
-from ev3dev2.sensor.lego import ColorSensor
-from ev3dev2.led import Leds
-from time import sleep
+from ev3dev2.motor import LargeMotor,MediumMotor, OUTPUT_D, OUTPUT_A,  OUTPUT_C, OUTPUT_B, follow_for_ms
+from ev3dev2.motor import SpeedDPS, SpeedRPM, SpeedRPS, SpeedDPM, MoveTank, MoveSteering, SpeedPercent
 from ev3dev2.sound import Sound
-from athenaRobot import AthenaRobot
-import random
+from time import sleep
+from ev3dev2.sensor.lego import ColorSensor
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4
+from ev3dev2.sound import Sound
+import math
+import sys 
+import time
+LeftAction      = MediumMotor(OUTPUT_A)
+RightAction     = MediumMotor(OUTPUT_D)
+TankPair        = MoveTank(OUTPUT_B, OUTPUT_C, motor_class=LargeMotor)
+LeftSensor      = ColorSensor(INPUT_1)
+RightSensor     = ColorSensor(INPUT_4)
+LeftWheel       = LargeMotor(OUTPUT_B)
+RightWheel      = LargeMotor(OUTPUT_C)
+sound           = Sound()
+def linefollowing2(FastSpeed,SlowSpeed,Degree):
+    DegreeSum = 0
+    AngleOld    = 360 * RightWheel.position / RightWheel.count_per_rot
+    while DegreeSum < Degree:
+        if RightSensor.color == ColorSensor.COLOR_WHITE:
+            RightWheel.on(SpeedDPS(FastSpeed))
+            LeftWheel.on(SpeedDPS(SlowSpeed))
+        else:
+            LeftWheel.on(SpeedDPS(FastSpeed))
+            RightWheel.on(SpeedDPS(SlowSpeed))  
+        AngleNew    = 360 * RightWheel.position / RightWheel.count_per_rot
+        DegreeSum   = DegreeSum + abs(AngleNew - AngleOld)
+        AngleOld    = AngleNew
+        #print(RightSensor.reflected_light_intensity,LeftSensor.reflected_light_intensity,file=sys.stderr)
+    LeftWheel.off()
+    RightWheel.off()
 
-def runTrip3():
-    sound = Sound()
-    # sound.speak('This is trip three. Currently for Calibration.', espeak_opts='-a 200 -s 130 -ven+m7' + 'en+whisper', volume=100)
-    robot = AthenaRobot()
-    robot.lineFollow(98, 15, 0.3)
+def Step7():
+    TankPair.on_for_degrees(SpeedDPS(-250),SpeedDPS(-250), 1400,True,True)
+    TankPair.on_for_degrees(SpeedDPS(250),SpeedDPS(250), 60,True,True)
+    LeftAction.on_for_degrees(SpeedDPS(-500),700,True,True)
+   
+def Step8():
+    while RightSensor.color !=6:
+        TankPair.on(SpeedDPS(-150),SpeedDPS(-150))
+    while RightSensor.color !=1:
+        TankPair.on(SpeedDPS(0),SpeedDPS(-150))
+    while LeftSensor.color !=1:
+        TankPair.on(SpeedDPS(-150),SpeedDPS(0))
+    LeftWheel.off()
+    RightWheel.off()
+    TankPair.on_for_degrees(SpeedDPS(-250),SpeedDPS(-250),250,True,True)
+    TankPair.on_for_degrees(SpeedDPS(0),SpeedDPS(-200),130,True,True)
+def Step9():
+    TankPair.on_for_degrees(SpeedDPS(200),SpeedDPS(200),120,True,True)
+    TankPair.on_for_degrees(SpeedDPS(200),SpeedDPS(0),50,True,True)
+    while RightSensor.color !=6:
+        TankPair.on(SpeedDPS(100),SpeedDPS(100))
+    while LeftSensor.color !=6:
+        TankPair.on(SpeedDPS(100),SpeedDPS(0))
+    while RightSensor.color !=1:
+        TankPair.on(SpeedDPS(0),SpeedDPS(100))
+    while LeftSensor.color !=1:
+        TankPair.on(SpeedDPS(100),SpeedDPS(0))
+    while RightSensor.color !=6:
+        TankPair.on(SpeedDPS(0),SpeedDPS(-100))
+    TankPair.on_for_degrees(SpeedDPS(0),SpeedDPS(-150),100,True,True)
+    while RightSensor.color !=1:
+        TankPair.on(SpeedDPS(-100),SpeedDPS(-100))
+    while RightSensor.color !=6:
+        TankPair.on(SpeedDPS(-100),SpeedDPS(-100))
+    TankPair.on_for_degrees(SpeedDPS(-200),SpeedDPS(-200),120,True,True)
+    while RightSensor.color !=6:
+        TankPair.on(SpeedDPS(0),SpeedDPS(100))
+    while RightSensor.color !=1:
+        TankPair.on(SpeedDPS(0),SpeedDPS(100))
+    LeftWheel.off()
+    RightWheel.off()
+    linefollowing2(-100,0,150)
+    while LeftSensor.color !=1:
+        TankPair.on(SpeedDPS(-150),SpeedDPS(-150))
+    TankPair.on_for_degrees(SpeedDPS(0),SpeedDPS(100),10,True,True)
+    TankPair.on_for_degrees(SpeedDPS(-200),SpeedDPS(-200),450,True,True)
 
-    
-    #Calibrating both sensors
+def runTrip3():  
+    Step7()
+    Step8()
+    #Step9()
 
-    #Done signal
-    
-
-#!/usr/bin/env pybricks-micropython
-""" from pybricks import ev3brick as brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import (Port, Stop, Direction, Button, Color, SoundFile, ImageFile, Align)
-from pybricks.tools import print, wait, StopWatch
-from pybricks.robotics import DriveBase
- 
-def runTrip3():
-    # Color Sensing
-    CS = ColorSensor(Port.S4)
-    if CS.color() == 1:
-        brick.display.text("Black",(60,50))
-    elif CS.color() == 2:
-        brick.display.text("Blue",(60,50))
-    elif CS.color() == 3:
-        brick.display.text("Green",(60,50))
-    elif CS.color() == 4:
-        brick.display.text("Yellow",(60,50))
-    elif CS.color() == 5:
-        brick.display.text("Red",(60,50))
-    elif CS.color() == 6:
-        brick.display.text("White",(60,50))
-    elif CS.color() == 7:
-        brick.display.text("Brown",(60,50))
-    else:
-        brick.display.text("None", (60,50))
-    wait(1000)
-    brick.display.clear() """
