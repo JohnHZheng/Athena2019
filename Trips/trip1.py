590e8f033d196c80b884d6c9fcfc332eac97c8fa
Chlooooooe <lolycheng@gmail.com>
2019-11-29 13:13:58 -0800

    trip 1 changes
    alignment needs fixing
    otherwise working

--- Trips/trip1.py
+++ Trips/trip1.py
@@ -22,16 +22,32 @@ def runTrip1():
     
     #end trip constants
 
+    # aligning the robot against crane
+
     robot.run(distanceCm = 10, speedCmPerSecond = 15) 
     robot.turnRightOnLeftWheel(degree = 50)
-    robot.run(25, 15) 
-    sleep(3)
-    robot.turnLeftOnRightWheel(50,15)  
-    sleep(3)
-    robot.turnUntilLeftWhite(True,10,white_threshold=white_threshold)
-    robot.lineFollow()
-    robot.run(20, 15) 
-    for x in range(3):
-        robot.turnLeftOnRightWheel(20)
-        robot.turnRightOnLeftWheel(20)
+    robot.run(27, 15) 
+    robot.turnLeftOnRightWheel(50, 20) 
+    robot.run(15, 15) 
+    for x in range(5):
+        robot.turnLeftOnRightWheel(20, speed = 15)
+        robot.turnRightOnLeftWheel(20, speed = 15)
+    robot.run(3, 15)
+    robot.turnRightOnLeftWheel(5, 2)
     
+    # putting blue block and bat in place
+
+    robot.moveMediumMotor(isLeft = False, speed = 100, degrees = 4000, block = False) 
+    
+    # lifting the crane
+
+    for x in range(3): 
+        robot.moveMediumMotor(isLeft = True, speed = 75, degrees = 450)
+        robot.moveMediumMotor(isLeft = True, speed = 75, degrees = -450)
+    robot.moveMediumMotor(isLeft = False, speed = 25, degrees = 3500, block = False) 
+    robot.moveMediumMotor(isLeft = True, speed = 50, degrees = mediumMotorUpDegrees) 
+
+    # moving back
+
+    # robot.run(-10, 15) 
+    # robot.moveMediumMotor(isLeft = True, speed = 40, degrees = mediumMotorDownDegrees)
