3721edced8020c1cb929f829797bae4f9cb6437e
Ethan263 <53958783+Ethan263@users.noreply.github.com>
2019-11-30 11:52:39 -0800

    chloes changes

--- Trips/trip1.py
+++ Trips/trip1.py
@@ -12,26 +12,40 @@ from athenaRobot import AthenaRobot
 def runTrip1(): 
     robot = AthenaRobot()
     # begin of trip constants
-    white_value_left = 88
+    white_value_left = 80
     white_value_right = 82
     black_value = 15
-    mediumMotorUpDegrees = 1000
-    mediumMotorDownDegrees = 1020
-    mediumMotorUpSpeed = -30 
-    mediumMotorDownSpeed = 50
+    mediumMotorUpDegrees = 800
+    mediumMotorDownDegrees = 800
+    mediumMotorUpSpeed = 30 
+    mediumMotorDownSpeed = -50
     
-    #end trip constants
+    # end trip constants
+
+    # aligning the robot against crane
 
     robot.run(distanceCm = 10, speedCmPerSecond = 15) 
     robot.turnRightOnLeftWheel(degree = 50)
-    robot.run(25, 15) 
-    sleep(3)
-    robot.turnLeftOnRightWheel(50,15)  
-    sleep(3)
-    robot.turnUntilLeftWhite(True,10,white_threshold=white_threshold)
-    robot.lineFollow()
-    robot.run(20, 15) 
-    for x in range(3):
-        robot.turnLeftOnRightWheel(20)
-        robot.turnRightOnLeftWheel(20)
-    
+    robot.run(27, 15) 
+    robot.turnLeftOnRightWheel(50, 20) 
+    robot.run(15, 15) 
+    for x in range(5):
+        robot.turnLeftOnRightWheel(20, speed = 15)
+        robot.turnRightOnLeftWheel(20, speed = 15)
+    robot.run(3, 15)
+    robot.turnRightOnLeftWheel(5, 2)
+
+    # putting blue block and bat in place
+    # robot.moveMediumMotor(isLeft = False, speed = 100, degrees = 4000, block = False) 
+
+    # # lifting the crane
+    # for x in range(3): 
+    #     robot.moveMediumMotor(isLeft = True, speed = 75, degrees = 450)
+    #     robot.moveMediumMotor(isLeft = True, speed = 75, degrees = -450)
+    # robot.moveMediumMotor(isLeft = False, speed = 25, degrees = 3500, block = False) 
+    # robot.moveMediumMotor(isLeft = True, speed = 50, degrees = mediumMotorUpDegrees) 
+
+    # moving back
+
+    # robot.run(-10, 15) 
+    # robot.moveMediumMotor(isLeft = True, speed = 40, degrees = mediumMotorDownDegrees)
