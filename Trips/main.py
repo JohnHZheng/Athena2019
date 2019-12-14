1c9cb5576015f4ecf955df39e032742f2ec9a32b
coding000 <53977813+coding000@users.noreply.github.com>
2019-12-13 19:12:30 -0800

    aaa

--- Trips/main.py
+++ Trips/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env micropython
 from ev3dev2.button import Button
 from ev3dev2.sound import Sound
+from ev3dev2.motor import LargeMotor,MediumMotor, OUTPUT_D, OUTPUT_A,  OUTPUT_C, OUTPUT_B, follow_for_ms
 from time import sleep
 from trip1 import runTrip1
 from trip2 import runTrip2
@@ -11,6 +12,13 @@ from time import sleep
 import os
 os.system('setfont Uni3-TerminusBold16')
 sound = Sound()
+LeftAction      = MediumMotor(OUTPUT_A)
+RightAction     = MediumMotor(OUTPUT_D)
+TankPair        = MoveTank(OUTPUT_B, OUTPUT_C, motor_class=LargeMotor)
+LeftSensor      = ColorSensor(INPUT_1)
+RightSensor     = ColorSensor(INPUT_4)
+LeftWheel       = LargeMotor(OUTPUT_B)
+RightWheel      = LargeMotor(OUTPUT_C)
 
 btn = Button()
 contin = True
@@ -22,7 +30,10 @@ print('4 - Down for Trip 4')
 print('5 - Center for Trip 5')
 sound.beep()
 while contin:
-
+    RightAction.reset()
+    LeftAction.reset()
+    LeftWheel.reset()
+    RightWheel.reset()
     if btn.left == True:
         runTrip1()
         sound.beep()
